082fe6667c0ab02230f11225a0fd495adb7aca6a
Bhargav <bhargavkadali39@gmail.com>
2021-09-28 20:24:57 +0530

    Update swappy.py

--- swappy.py
+++ swappy.py
@@ -1,61 +1,68 @@
 
-def type1():
-    x,y=10,20
+def breaky():
+    print('------------------')
+
+def type1(x,y):
     print('Type 1')
+    print('resetting values')
+    print('x: {0} y: {1}\n'.format(x,y))
     x = x + y
     y = x - y
     x = x - y
+    print('After swapping')
     print('x: {0} y: {1}\n'.format(x,y))
-    print('------------------')
+    breaky()
 
 
-def type2():
-    x,y=10,20
+def type2(x,y):
     print('Type 2')
+    print('resetting values')
+    print('x: {0} y: {1}\n'.format(x,y))
     x = x * y
     y = x / y
     x = x / y
+    print('After swapping')
     print('x: {0} y: {1}\n'.format(x,y))
-    print('------------------')
+    breaky()
 
 
-def type3():
-    x,y=10,20
+def type3(x,y):
     print('Type 3')
+    print('resetting values')
+    print('x: {0} y: {1}\n'.format(x,y))
     x = (x & y) + (x | y)
     y = x + (~y) + 1
     x = x + (~y) + 1
+    print('After swapping')
     print('x: {0} y: {1}\n'.format(x,y))
-    print('------------------')
+    breaky()
 
 
-def type4():
-    x,y=10,20
+def type4(x,y):
     print('Type 4')
+    print('resetting values')
+    print('x: {0} y: {1}\n'.format(x,y))
     x = x ^ y
     y = x ^ y
     x = x ^ y
+    print('After swapping')
     print('x: {0} y: {1}\n'.format(x,y))
-    print('------------------')
+    breaky()
 
 
-def type5():
-    x,y=10,20
+def type5(x,y):
     print('Type 5')
+    print('resetting values')
+    print('x: {0} y: {1}\n'.format(x,y))
     x,y = y,x
+    print('After swapping')
     print('x: {0} y: {1}\n'.format(x,y))
-    print('------------------')
+    breaky()
 
-def type6():
-    print('Type6')
-    x,y=10,20
-    x,y=y,x
-    print('x: {0} y: {1}\n'.format(x,y))
 
-type1()
-type2()
-type3()
-type4()
-type5()
-type6()
-
+breaky()
+type1(10,20)
+type2(10,20)
+type3(10,20)
+type4(10,20)
+type5(10,20)
