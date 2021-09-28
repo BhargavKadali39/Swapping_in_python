944dc7893604686c539b722cbde73ebbaca1e7ac
Bhargav <bhargavkadali39@gmail.com>
2021-09-28 20:21:31 +0530

    Update swappy.py

--- swappy.py
+++ swappy.py
@@ -1,1 +1,51 @@
 
+def type1(x,y):
+    print('Type 1')
+    x = x + y
+    y = x - y
+    x = x - y
+    print('x: {0} y: {1}\n'.format(x,y))
+    print('------------------')
+
+
+def type2(x,y):
+    print('Type 2')
+    x = x * y
+    y = x / y
+    x = x / y
+    print('x: {0} y: {1}\n'.format(x,y))
+    print('------------------')
+
+
+def type3(x,y):
+    print('Type 3')
+    x = (x & y) + (x | y)
+    y = x + (~y) + 1
+    x = x + (~y) + 1
+    print('x: {0} y: {1}\n'.format(x,y))
+    print('------------------')
+
+
+def type4(x,y):
+    print('Type 4')
+    x = x ^ y
+    y = x ^ y
+    x = x ^ y
+    print('x: {0} y: {1}\n'.format(x,y))
+    print('------------------')
+
+
+def type5(x,y):
+    print('Type 5')
+    x,y = y,x
+    print('x: {0} y: {1}\n'.format(x,y))
+    print('------------------')
+
+
+
+type1()
+type2()
+type3()
+type4()
+type5()
+
