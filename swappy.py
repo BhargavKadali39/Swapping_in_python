693d75d9fcfd1142a79afe27b02f4771c5460c74
Bhargav <bhargavkadali39@gmail.com>
2021-09-28 20:18:29 +0530

    Update swappy.py

--- swappy.py
+++ swappy.py
@@ -1,1 +1,21 @@
 
+
+mytuxle = ('x = x + y',
+           'y = x - y',
+           'x = x - y',
+           
+           'x = x * y',
+           'y = x / y',
+           'x = x / y',
+           
+           'x = x * y',
+           'y = x / y',
+           'x = x / y',
+           
+           'x = (x & y) + (x | y)',
+           'y = x + (~y) + 1',
+
+           'x = x + (~y) + 1')
+
+
+for i in mytuxle:
